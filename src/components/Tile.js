745bc8988d7c8ab8a3ebb1606e0a462ebf057025
vibhu mahendru <vibhumahendru@vibhus-MacBook-Pro.local>
2019-01-10 15:43:30 -0500

    Done.

--- src/components/Tile.js
+++ src/components/Tile.js
@@ -4,18 +4,53 @@ import React, { Component } from 'react';
 //   let name=props.name.toLowerCase()
 //   return <img src={name}>
 // }
+let weightKey1='weight as a ratio of hog to LG - 24.7 Cu. Ft. French Door Refrigerator with Thru-the-Door Ice and Water'
+
+
+function Hoginfo(props){
+
+  if (props.clicked){
+    return <p>{props.hogObj.specialty} <br></br>Greased? {props.hogObj.greased.toString()} Highest Medal Achieved {props.hogObj['highest medal achieved']} Weight: {props.hogObj[weightKey1]}</p>
+  }else{return null}
+}
 
 class Tile extends Component {
 
+  state = {
+    clicked: false,
+    hidden: false
+  }
+
+  handleClick = () => {
+    let state= this.state.clicked ? false : true
+    this.setState({
+        clicked: state
+      })
+    }
+
+  handleHide = () => {
+    this.setState({
+      hidden: true
+    })
+  }
+
 correctImg = (name) => {
-  name.toLowerCase(this.props.hogObj.name)
+  let normalName = name.toLowerCase()
+  let newNormalName = normalName.split(' ').join('_');
+  return newNormalName
 }
 
+
   render() {
+
     return (
-      <div className="pigTile">
+      <div className={`pigTile`}>
+      <button onClick={this.handleHide}>HIDE ME</button>
+      <div onClick={this.handleClick} className={this.state.hidden ? "hide" : ""} >
         <p>{this.props.hogObj.name}</p>
-        <img src={require("../hog-imgs/cherub.jpg")}/>
+        <img src={require(`../hog-imgs/${this.correctImg(this.props.hogObj.name)}.jpg`)}/>
+        <Hoginfo clicked={this.state.clicked} hogObj={this.props.hogObj} />
+      </div>
       </div>
     );
   }
