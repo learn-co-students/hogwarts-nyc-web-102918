1eb70f818e035a146780c2a462c5dc58401ceba5
jbmenashi <jbmenashi@gmail.com>
2019-01-11 09:49:57 -0500

    Done.

--- src/components/App.js
+++ src/components/App.js
@@ -2,13 +2,70 @@ import React, { Component } from 'react';
 import '../App.css';
 import Nav from './Nav'
 import hogs from '../porkers_data';
+import HogContainer from './HogContainer'
 
 class App extends Component {
+
+  state = {
+    hogs: hogs,
+    isButtonClicked: false,
+  }
+
+  handleClickGreased = () => {
+    let greasedHogs = this.state.hogs.filter(hog => hog.greased)
+    if (!this.state.isButtonClicked) {
+      this.setState({
+        hogs: greasedHogs,
+        isButtonClicked: !this.state.isButtonClicked
+      })
+    }
+    else {
+      this.setState({
+        hogs: hogs,
+        isButtonClicked: !this.state.isButtonClicked
+      })
+    }
+  }
+
+  handleChange = event => {
+    let alphaHogs = [...this.state.hogs].sort((a, b) => {
+      return a.name.localeCompare(b.name);
+    })
+
+    let weightHogs = [...this.state.hogs].sort((a, b) => b['weight as a ratio of hog to LG - 24.7 Cu. Ft. French Door Refrigerator with Thru-the-Door Ice and Water'] - a['weight as a ratio of hog to LG - 24.7 Cu. Ft. French Door Refrigerator with Thru-the-Door Ice and Water'])
+
+    if (event.target.value === "all") {
+      this.setState({
+        hogs: hogs
+      })
+    }
+    else if (event.target.value === "name") {
+      this.setState({
+        hogs: alphaHogs
+      })
+    }
+    else if (event.target.value === "weight") {
+      this.setState({
+        hogs: weightHogs
+      })
+    }
+  }
+
   render() {
+    let greasedChecker
+
+    !this.state.isButtonClicked ? greasedChecker = <button onClick={this.handleClickGreased} >Show Greased Pigs</button> : greasedChecker = <button onClick={this.handleClickGreased} >Show All Pigs</button>
+
     return (
       <div className="App">
           < Nav />
-
+          <select onChange={this.handleChange}>
+            <option value="all">Sort By NOTHING</option>
+            <option value="name">Sort By Name</option>
+            <option value="weight">Sort By Weight</option>
+          </select>
+          {greasedChecker}
+          < HogContainer hogs={this.state.hogs}/>
       </div>
     )
   }
---
+++ src/components/HogTile.js
@@ -0,0 +1,45 @@
+import React, { Component } from 'react';
+
+class HogTile extends Component {
+
+  state = {
+    isClicked: false
+  }
+
+  getHogName = (hogname) => {
+    hogname = hogname.split(" ")
+    let lowerHogName = hogname.map(word => word.toLowerCase())
+    return lowerHogName.join("_") + ".jpg"
+  }
+
+  handleClick = () => {
+    this.setState({
+      isClicked: !this.state.isClicked
+    })
+  }
+
+  render() {
+    let displayCard
+    if (!this.state.isClicked) {
+      displayCard = <img src={ require(`../hog-imgs/${this.getHogName(this.props.name)}`) } alt=""/>
+    }
+    else {
+      displayCard = <div>
+                      <h3>Specialty: {this.props.specialty}</h3>
+                      <h3>Weight: {this.props['weight as a ratio of hog to LG - 24.7 Cu. Ft. French Door Refrigerator with Thru-the-Door Ice and Water']}</h3>
+                      <h3>Medal: {this.props['highest medal achieved']}</h3>
+                      <h3>Greased? {this.props.greased ? "Yep" : "Nah"}</h3>
+                    </div>
+    }
+
+    return (
+      <div className="pigTile" onClick={this.handleClick}>
+        <h1>{this.props.name}</h1>
+        {displayCard}
+      </div>
+    );
+  }
+
+}
+
+export default HogTile;
